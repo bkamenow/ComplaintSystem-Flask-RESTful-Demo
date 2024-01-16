32cb61ddaf54e24d2815e615c47a16ce6c200d25
Borislav Kamenov <kamenowink@gmail.com>
2024-01-16 20:30:59 +0200

    nest user inside complaint schema and make complaints schema work with list

--- schemas/base.py
+++ schemas/base.py
@@ -6,6 +6,13 @@ class UserRequestBase(Schema):
     password = fields.String(required=True)
 
 
+class UserResponseBase(Schema):
+    id = fields.Integer(dump_only=True)
+    first_name = fields.String(required=True)
+    last_name = fields.String(required=True)
+    email = fields.Email(required=True)
+
+
 class ComplaintBase(Schema):
     title = fields.String(required=True)
     description = fields.String(required=True)
--- schemas/response_schemas/complaints.py
+++ schemas/response_schemas/complaints.py
@@ -1,7 +1,7 @@
 from marshmallow import fields, Schema
 
 from models import State
-from schemas.base import ComplaintBase
+from schemas.base import ComplaintBase, UserResponseBase
 
 
 class ComplaintResponseSchema(ComplaintBase):
@@ -9,9 +9,10 @@ class ComplaintResponseSchema(ComplaintBase):
     created_at = fields.DateTime(required=True)
     status = fields.Enum(State, by_value=True)
     user_id = fields.Integer(required=True)
-    # TODO: nest user inside this schema
+    # DONE TODO: nest user inside this schema
+    user = fields.Nested(UserResponseBase(), only=("id", "first_name", "last_name", "email"))
 
 
 class ComplaintsResponseSchema(Schema):
-    # TODO: make schema working with list of  Complaints schema
-    complaints = fields.Nested(ComplaintResponseSchema, many=True)
+    # DONE TODO: make schema working with list of  Complaints schema
+    complaints = fields.List(fields.Nested(ComplaintResponseSchema()), data_key='complaints')
