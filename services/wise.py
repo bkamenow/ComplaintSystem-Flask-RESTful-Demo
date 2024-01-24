9939410d192b92a0ef325c86cb78139e66259058
Borislav Kamenov <kamenowink@gmail.com>
2024-01-24 20:24:36 +0200

    fix the create transfer problem

--- services/wise.py
+++ services/wise.py
@@ -1,8 +1,6 @@
 import json
 import uuid
-
 import requests
-
 from werkzeug.exceptions import InternalServerError
 from decouple import config
 
@@ -14,9 +12,7 @@ class WiseService:
             "Content-Type": "application/json",
             "Authorization": f"Bearer {config('WISE_TOKEN')}",
         }
-        profile_id = self._get_profile_id()
-        self.profile_id = profile_id
-        print(profile_id)
+        self.profile_id = self._get_profile_id()
 
     def _get_profile_id(self):
         url = self.main_url + "/v2/profiles"
@@ -27,23 +23,23 @@ class WiseService:
             return resp.json()[0]['id']
         except (IndexError, KeyError):
             print(f"Error extracting profile ID. Response content: {resp.content}")
-            raise
+            raise InternalServerError("Unable to obtain profile ID")
 
     def create_quote(self, amount):
-        url = self.main_url + "/v3/quotes"
-        body = {
-            "sourceCurrency": "GBP",
-            "targetCurrency": "USD",
-            "sourceAmount": 200
+        url = self.main_url + "/v2/quotes"
+        data = {
+            "sourceCurrency": "EUR",
+            "targetCurrency": "BGN",
+            "targetAmount": amount,
+            "profile": self.profile_id,
         }
-
-        resp = requests.post(url, headers=self.headers, json=body)
+        resp = requests.post(url, headers=self.headers, data=json.dumps(data))
 
         if resp.status_code == 200:
             resp = resp.json()
             return resp["id"]
         else:
-            print(resp.content)
+            print(resp)
             raise InternalServerError("Payment provider is not available at the moment")
 
     def create_recipient_account(self, full_name, iban):
@@ -61,17 +57,14 @@ class WiseService:
         }
         resp = requests.post(url, json=body, headers=self.headers)
 
-        if resp.status_code == 200:
-            resp = resp.json()
-            return resp["id"]
-        elif resp.status_code == 422:
-            print(resp.content)  # Print response content for 422 Unprocessable Entity
-        elif resp.status_code == 500:
-            print(resp.content)  # Print response content for 500 Internal Server Error
-            raise InternalServerError("Payment provider is not available at the moment")
-        else:
-            print(resp)
-            raise InternalServerError("Unexpected error during recipient creation")
+        try:
+            resp.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(f"Recipient account creation failed. Response content: {resp.content}")
+            raise err
+
+        recipient_id = resp.json()["id"]
+        return recipient_id
 
     def create_transfer(self, target_account_id, quote_id):
         customer_transaction_id = str(uuid.uuid4())
@@ -81,33 +74,34 @@ class WiseService:
             "targetAccount": target_account_id,
             "quoteUuid": quote_id,
             "customerTransactionId": customer_transaction_id,
-            "details": {},
+            "details": {
+                "reference": "to my friend",
+            },
         }
 
         resp = requests.post(url, headers=self.headers, json=body)
 
-        if resp.status_code == 422:
-            print("Error creating transfer. Response content:")
-            print(resp.content)
-        elif resp.status_code == 200:
-            resp = resp.json()
-            return resp
-        else:
-            print(resp)
-            raise InternalServerError("Unexpected error during transfer creation")
+        try:
+            resp.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(f"Transfer creation failed. Response content: {resp.content}")
+            raise err
 
-        return resp
+        transfer_id = resp.json()["id"]
+        return transfer_id
 
     def fund_transfer(self, transfer_id):
         url = self.main_url + f"/v3/profiles/{self.profile_id}/transfers/{transfer_id}/payments"
         resp = requests.post(url, headers=self.headers)
 
-        if resp.status_code == 200:
-            resp = resp.json()
-            return resp["id"]
-        else:
-            print(resp)
-            raise InternalServerError("Payment provider is not available at the moment")
+        try:
+            resp.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(f"Transfer funding failed. Response content: {resp.content}")
+            raise err
+
+        payment_id = resp.json()["id"]
+        return payment_id
 
 
 if __name__ == "__main__":
@@ -119,4 +113,3 @@ if __name__ == "__main__":
     print(q_id)
     print(recipient_id)
     print(transfer)
-
